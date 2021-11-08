1c8d0132f6421f6300ce75fcc9eb1c31f8466c6e
lnalmeida <l.n.almeida.ti@gmail.com>
2021-11-08 03:14:27 -0300

    deploy in ghp

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   url: 'https://api-todo-angular.herokuapp.com/',
 };
